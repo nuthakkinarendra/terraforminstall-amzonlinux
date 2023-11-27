90c13ac318d2bfedf1de4e4e3883cb5436152320
nuthakkinarendra <70637778+nuthakkinarendra@users.noreply.github.com>
2023-11-27 12:38:05 +0530

    Update terraformv2project.tf

--- terraformv2project.tf
+++ terraformv2project.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-02a2af70a66af6dfb"
   instance_type = "t2.micro"
